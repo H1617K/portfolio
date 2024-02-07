afaba4b4c2c00aac56986c34e244e8acd30b2306
harsh <harsh@WHOHARSH>
2024-02-07 10:41:54 +0530

    give animation and responciv css to Nvabar and Services

--- src/Conponent/Navbar.js
+++ src/Conponent/Navbar.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../Styled/Navbar.css';
 
 const Navbar = () => {
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand-lg navbar-light bg-dark">
         <Link className="navbar-brand" to="#">Force Web Tech</Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
@@ -12,26 +13,26 @@ const Navbar = () => {
 
         <div className="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
           <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link className="nav-link" to="Service">Services <span className="sr-only">(current)</span></Link>
+            <li className="nav-item active">
+              <Link className="nav-link" to="Service">Services</Link>
             </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="Product">Products <span className="sr-only">(current)</span></Link>
+            <li className="nav-item active">
+              <Link className="nav-link" to="Product">Products</Link>
             </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="Slider">Slider <span className="sr-only">(current)</span></Link>
+            <li className="nav-item active">
+              <Link className="nav-link" to="Slider">Slider</Link>
             </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="Blog">Blogs <span className="sr-only">(current)</span></Link>
+            <li className="nav-item active">
+              <Link className="nav-link" to="Blog">Blogs</Link>
             </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="Testimonials">Testimonials <span className="sr-only">(current)</span></Link>
+            <li className="nav-item active">
+              <Link className="nav-link" to="Testimonials">Testimonials</Link>
             </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="FAQ">FAQs <span className="sr-only">(current)</span></Link>
+            <li className="nav-item active">
+              <Link className="nav-link" to="FAQ">FAQs</Link>
             </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="AboutUS">About Us <span className="sr-only">(current)</span></Link>
+            <li className="nav-item active">
+              <Link className="nav-link" to="AboutUS">About Us</Link>
             </li>
           </ul>
         </div>
