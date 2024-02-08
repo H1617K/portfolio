8eae63b0d841400b8dc93d780a5bec25aa5b9581
harsh <harsh@WHOHARSH>
2024-02-08 12:25:46 +0530

    set Aboutpage with css, animations and responsuv

--- src/Conponent/AboutUs.js
+++ src/Conponent/AboutUs.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../Styled/AboutUs.css';
-import { team } from '../Images/Images';
+import '@fortawesome/fontawesome-free/css/all.css';
+import { ACCOUNTABILITY, COMMITMENT1, EXCELLENCE, Efficiency, INNOVATION, LevelofCommitment, Qualitycode, TRANSPARENCY, team } from '../Images/Images';
 
 const AboutUs = () => {
 
@@ -8,22 +9,22 @@ const AboutUs = () => {
     {
       title: "1. Discover",
       description:"The Discover phase is all about discovering the variety of growth possibilities at your fingertips. By assessing the current state of your people, processes, and technology, we listen to your pain areas and assess what can set you on the path for growth.",
-      icon:"fa-file-magnifying-glass",
+      icon:"fa-solid fa-chart-line-up",
     },
     {
       title: "2. Define",
       description:"The Define phase is designing a game plan. With your goals in mind, we brainstorm a solution and scope out the options that best suit your business’ functional and technical needs.",
-      icon:"fa-laptop-arrow-down",
+      icon:"fa-regular fa-laptop-arrow-down",
     },
     {
       title: "3. Implement",
       description:"The Implement phase is where the rubber meets the road. We work with your team to ensure that the solutions we define are effectively and efficiently executed. This phase is never a one-and-done transaction – we ensure that you’re satisfied with the results of implementation.",
-      icon:"fa-gear-code",
+      icon:"fa-regular fa-gear-code",
     },
     {
       title: "4. Growth",
       description:"The Growth phase is where the magic happens! We will continue to work and engage with you to ensure that your newfound solution is adding the value your business needs to grow.",
-      icon:"fa-chart-line-up",
+      icon:"fa-solid fa-chart-line-up",
     },
   ]
 
@@ -38,8 +39,8 @@ const AboutUs = () => {
             We are here to transform your ideas into reality. 
             </p>
             <p>
-              Our technical experts are ready to take next challenge.
-              </p>
+            Our technical experts are ready to take next challenge.
+            </p>
           </div>
           <div className='ImageHand'>
             <img src={team} alt='team' />
@@ -59,11 +60,11 @@ const AboutUs = () => {
             success record with our happy customers!</p>
           </div>
         </div>
-        <br />
+        <br /><br />
         <div className='fourphases'>
           <div className='Header-fourphases'>
             <p>We have divided our process into four phases: Discover, Define, Implementation, and Growth.</p>
-          </div>
+          </div><br />
           <div className='Phases'>
             <div className="Phases-list">
               {Phases.map((phase, index) => (
@@ -80,6 +81,63 @@ const AboutUs = () => {
             </div>
           </div>
         </div>
+        <br /><br/>
+          <div className='Businessplans'>
+            <br/>
+            <div className='Paragraph'>
+              <p>
+              ForceWebTech is a tech consulting company, specialized in building and integrating a wide range of web and<br />
+              mobile solutions. Our objective is to deliver robust, scalable, innovative, and quality solutions with strict deadlines<br />
+              in line with your business plans.
+              </p>
+            </div><hr />
+            <div className='ParahraphThings'>
+              <div className='QualityOfCode'>
+              <img src={Qualitycode} alt='Qualitycode' />
+              <p>Quality of Code.</p>
+              </div>
+              <div className='Communication'>
+              <img src={Efficiency} alt='Communication' />
+              <p>Efficiency of Communication.</p>
+              </div>
+              <div className='LevelofCommitment'>
+              <img src={LevelofCommitment} alt='LevelofCommitment' />
+              <p>Level  of Commitment. </p>
+              </div>
+            </div>
+          </div>
+          <br /><br/><br/>
+          <div className='CoreValus'>
+              <div className='Paragraph1'>
+                <p>
+                Honesty, Ethics and Respect are the pillars that ForceWebTech follow for business collaboration. Our core values are the guiding principles upon we <br />
+                was founded and how we strive to conduct our business on a daily basis.
+                </p>
+                <br/>
+                <div className='ParahraphThings1'>
+                  <div className='EXCELLENCE'>
+                  <img src={EXCELLENCE} alt='EXCELLENCE' />
+                  <p>EXCELLENCE</p>
+                  </div>
+                  <div className='INNOVATION'>
+                  <img src={INNOVATION} alt='INNOVATION' />
+                  <p>INNOVATION</p>
+                  </div>
+                  <div className='TRANSPARENCY'>
+                  <img src={TRANSPARENCY} alt='TRANSPARENCY' />
+                  <p>TRANSPARENCY </p>
+                  </div>
+                  <div className='COMMITMENT1'>
+                  <img src={COMMITMENT1} alt='COMMITMENT1' />
+                  <p>COMMITMENT </p>
+                  </div>
+                  <div className='ACCOUNTABILITY'>
+                  <img src={ACCOUNTABILITY} alt='ACCOUNTABILITY' />
+                  <p>ACCOUNTABILITY </p>
+                  </div>
+                </div>
+              </div>
+            </div>
       </div>
     </section>
   );
--- src/Images/Images.js
+++ src/Images/Images.js
@@ -12,8 +12,17 @@ import Slider3 from '../assets/Slider3.jpg'
 import ServicesBack from '../assets/ServicesBack.JPG'
 import JoinHand from '../assets/JoingHands.png'
 import team from '../assets/team.jpg'
+import Qualitycode from '../assets/Qualitycode.png'
+import Efficiency from '../assets/Asurence.png'
+import LevelofCommitment from '../assets/Commitment.jpg'
+import EXCELLENCE from '../assets/EXCELLENCE.png'
+import INNOVATION from '../assets/INNOVATION.png'
+import TRANSPARENCY from '../assets/TRANSPARENCY & INTEGRITY.jpg'
+import COMMITMENT1 from '../assets/COMMITMENT1.jpg'
+import ACCOUNTABILITY from '../assets/ACCOUNTABILITY.png'
 
 export {
     CloudComputingGuide,futureofmobiledevelopment,WebDevelopmentTrends,WebDevelopment,
-    MobileappDevelopment,Cloudservices,HarshPhoto,Harsh1,Silder1,Slider2,Slider3,ServicesBack,JoinHand,team
+    MobileappDevelopment,Cloudservices,HarshPhoto,Harsh1,Silder1,Slider2,Slider3,ServicesBack,JoinHand,team,Qualitycode,Efficiency,
+    LevelofCommitment,EXCELLENCE,INNOVATION,TRANSPARENCY,COMMITMENT1,ACCOUNTABILITY
 }
