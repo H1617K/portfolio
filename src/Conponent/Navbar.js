851d71dbbcbf823b1bf80c6de0849c0f4175cbf6
harsh <harsh@WHOHARSH>
2024-02-08 18:08:42 +0530

    Minor change in Navbar color

--- src/Conponent/Navbar.js
+++ src/Conponent/Navbar.js
@@ -5,7 +5,7 @@ import '../Styled/Navbar.css';
 const Navbar = () => {
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+      <nav className="navbar navbar-expand-lg navbar-light bg-white">
         <Link className="navbar-brand" to="Service"></Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
