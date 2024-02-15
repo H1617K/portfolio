5ab6c998ffd1a127d4490f8e8a148bab6b5dc8ee
harsh <harsh@WHOHARSH>
2024-02-15 12:07:31 +0530

    configuer map set only on countactus page

--- src/Conponent/ContactUs.js
+++ src/Conponent/ContactUs.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom'; 
 import emailjs from 'emailjs-com';
 import '../Styled/ContactUs.css';
 
@@ -6,6 +7,18 @@ const ContactUs = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
+  const location = useLocation(); 
+
+  const isContactPage = location.pathname === '/ContactUs'; 
+
+  useEffect(() => {
+    // Reset form fields when component unmounts (not strictly necessary, but good practice)
+    return () => {
+      setName('');
+      setEmail('');
+      setMessage('');
+    };
+  }, []);
 
   const sendEmail = (e) => {
     e.preventDefault();
@@ -48,11 +61,12 @@ const ContactUs = () => {
       </div>
     </div>
     <br />
-    <div className='GoogleMap'>
-    <iframe title="Google Map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d918.2337036342235!2d72.
-    49634316955164!3d22.989424398699814!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xa6947d1f54949a59%3A0xc90c18118973530d!2sForcewebtech!5e0!3m2!1sen!2sin!4v1707972618685!5m2!1sen!2sin"
-     width="1500" height="400" style={{ border: "0" }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-    </div>
+    {isContactPage && (
+      <div className='GoogleMap'>
+        <iframe title="Google Map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d918.2337036342235!2d72.49634316955164!3d22.989424398699814!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xa6947d1f54949a59%3A0xc90c18118973530d!2sForcewebtech!5e0!3m2!1sen!2sin!4v1707972618685!5m2!1sen!2sin"
+          width="1500" height="400" style={{ border: "0" }} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+      </div>
+    )}
     </>
   );
 };
